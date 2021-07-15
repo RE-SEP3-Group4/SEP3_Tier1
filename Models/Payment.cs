f90f56144fe53d6bdfcd805ec6f5ee0fe332da9b
Bernardo Dias <55239586+joaobagas@users.noreply.github.com>
2021-07-15 10:57:54 +0200

    Created the Payment and reservation classes

---
+++ Models/Payment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEP3_Tier1.Models
+{
+    /// <summary>
+    /// This class holds the information regarding gym payments.
+    /// Payments contain the user id, a date intager with the ddmmyyhhmm format and a period of time it is valid for.
+    /// </summary>
+    public class Payment
+    {
+        int userID { get; set; }
+        int date { get; set; }
+        int period { get; set; }
+
+        public Payment() { }
+
+        public Payment(int userID, int date, int period)
+        {
+            this.userID = userID;
+            this.date = date;
+            this.period = period;
+        }
+    }
+}
---
+++ Models/Reservation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEP3_Tier1.Models
+{
+    /// <summary>
+    /// This class holds the information regarding gym reservation.
+    /// Reservations contain the user id and a date intager with the ddmmyyhhmm format.
+    /// Reservations are made in segments of one hour.
+    /// </summary>
+    public class Reservation
+    {
+        int userID { get; set; }
+        int date { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int userID, int date)
+        {
+            this.userID = userID;
+            this.date = date;
+        }
+    }
+}
